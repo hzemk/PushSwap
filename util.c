2d0e59923c3515a1cfdddd6ff1ef6d470b7f4839
leen9 <leenob006@icloud.com>
2026-02-22 01:23:45 +0300

    li

--- util.c
+++ util.c
@@ -9,19 +9,63 @@
 /*   Updated: 2026/02/08 15:47:28 by hal-taha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   util.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hal-taha <hal-taha@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/08 15:42:36 by hal-taha          #+#    #+#             */
+/*   Updated: 2026/02/08 15:47:28 by hal-taha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int	find_top(t_stack *a)
+#include "pushswap.h"
+
+int	find_bottom(t_stack *stack)
 {
-	int	i;
+	t_node	*current;
 
-	i = 1;
-	while (i < a->size)
-	{
-		a = a->next;
-		i++;
-	}
-	return(a-> value);
+	if (!stack || !stack->top)
+		return (0);
+	current = stack->top;
+	while (current->next)
+		current = current->next;
+	return (current->value);
 }
+
+int	find_top(t_stack *stack)
+{
+	if (!stack || !stack->top)
+		return (0);
+	return (stack->top->value);
+}
+
+void	push_back(t_stack *stack, int value)
+{
+	t_node	*new;
+	t_node	*current;
+
+	if (!stack)
+		return ;
+	new = malloc(sizeof(t_node));
+	if (!new)
+		error(stack);
+	new->value = value;
+	new->next = NULL;
+	if (!stack->top)
+		stack->top = new;
+	else
+	{
+		current = stack->top;
+		while (current->next)
+			current = current->next;
+		current->next = new;
+	}
+	stack->size++;
+}
+
 int strcmp(char *s, char *j)
 {
         int i;
@@ -34,3 +78,4 @@ int strcmp(char *s, char *j)
         return (s[i] - j[i]);
 
 }
+
